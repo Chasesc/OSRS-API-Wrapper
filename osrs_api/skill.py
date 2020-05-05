30187dc160c3a2b049c70fa214fba4e80e8f5c19
Chase Schelthoff <schelthoff.chase@gmail.com>
2020-05-04 23:13:25 -0400

    fix bug when calling xp_tnl while maxed

--- osrs_api/skill.py
+++ osrs_api/skill.py
@@ -11,11 +11,16 @@ class Skill(object):
         self.level = level
         self.xp = xp
 
+    # TODO: Add virtual levels to the XP_TABLE
     # xp to next level
     def xp_tnl(self):
+        if self.level >= 99: 
+            return 0 # already maxed, no real next level
         return const.XP_TABLE[self.level] - self.xp
 
     def xp_to(self, level):
+        if level > 99:
+            return 0 # same reason as above.
         return const.XP_TABLE[level - 1] - self.xp
 
     def __str__(self):
