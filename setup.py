c41a29a5badd01dacb5e3fe2ded9d01d8889a7b8
Chase Schelthoff <schelthoff.chase@gmail.com>
2020-05-04 23:16:51 -0400

    0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python_osrsapi",
-    version="0.0.1",
+    version="0.0.2",
     author="Chasesc",
     description="Oldschool Runescape API wrapper",
     long_description=long_description,
